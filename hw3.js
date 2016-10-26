c5f8569b3a0f7b97b303b947870fa91eb8d1223b
sjnguyen <sjn909@gmail.com>
2016-10-26 00:01:45 -0500

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,16 @@
+//quesiton 1
+var i = 0;
+while (i<101) {
+  console.log(i);
+  i++;
+}
+
+
+if (i % 3) {
+    console.log("fizz");
+} else if (i % 5) {
+    console.log("buzz");
+}
+  else {
+    console.log("Good evening");
+}
