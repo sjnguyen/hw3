6487e06ec524cb9361ef1f698fba4b229619d7d5
sjnguyen <sjn909@gmail.com>
2016-10-26 14:25:20 -0500

    Add files via upload

--- factorialize.js
+++ factorialize.js
@@ -1,6 +1,18 @@
-var n = 1
-function factorialize(num) {
-  return num;
-}
+// factorialize(5) should return a number.
+// factorialize(5) should return 120.
+// factorialize(10) should return 3628800.
+// factorialize(20) should return 2432902008176640000.
+// factorialize(0) should return 1.
 
-console.log(factorialize(1));
+
+var num =0
+function factorialize(num) {
+  if (num < 0)
+        return -1;
+  else if (num == 0)
+      return 1;
+  else {
+      return (num * factorialize(num - 1));
+  }
+}
+console.log(factorialize(num));
