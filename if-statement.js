77052a679bf1ba3fa749486d9a8cac6d705babad
sjnguyen <sjn909@gmail.com>
2016-10-26 10:50:37 -0500

    Add files via upload

---
+++ if-statement.js
@@ -0,0 +1,13 @@
+var fruit = 'orange'
+
+function length(str){
+  return str.length
+
+}
+
+
+if ('orange' > 5) {
+    console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has more than five characters.");
+}
