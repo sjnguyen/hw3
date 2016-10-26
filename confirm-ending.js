c8dfa9431fec63aa94791ca1e7295982ef8a8763
sjnguyen <sjn909@gmail.com>
2016-10-26 14:38:34 -0500

    Add files via upload

--- confirm-ending.js
+++ confirm-ending.js
@@ -1,3 +1,12 @@
+// confirmEnding("Bastian", "n") should return true.
+// confirmEnding("Connor", "n") should return false.
+// confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") should return false.
+// confirmEnding("He has to give me a new name", "name") should return true.
+// confirmEnding("Open sesame", "same") should return true.
+// confirmEnding("Open sesame", "pen") should return false.
+// confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") should return false.
+// Do not use the built-in method .endsWith() to solve the challenge.
+
 function end(str, target) {
   var start = str.length - (target.length - 1);
   if(str.substr(start, str.length) == target){
