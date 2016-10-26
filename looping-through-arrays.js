9e2577bbe5b3a7990d7e5aa5d54d08cdd66c2ce3
sjnguyen <sjn909@gmail.com>
2016-10-26 11:24:08 -0500

    Add files via upload

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,10 +1,6 @@
-
-
-var first_name = "peter";
-var last_name = "jones";
-var name=first_name.concat(last_name)
-
-    for (var member in list) {
-            text.concat(list[member]);
-    }
-    console.log(name)
+ var pets = [
+   'cat' + 's',
+   'dog' + 's',
+   'rat' + 's'
+ ];
+ console.log(pets);
